659a9541f091595b3bd196dafe31dcd199855fff
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-09-02 18:53:23 +0000

    mesures the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - get size of binary tree
+ * @tree: the tree
+ *
+ * Return: size_t size or NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
