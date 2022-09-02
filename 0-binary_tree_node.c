aa6c080f7723a02280ccd5f948a8e5407f578854
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-09-02 17:19:12 +0000

    creates a binary tree code

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: * to thee parent of the node to create
+ * @value: the value to add at the node
+ *
+ * Return: * to the new node or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	node = calloc(1, sizeof(binary_tree_t));
+
+	if(!node)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	return (node);
+}
