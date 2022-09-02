6516f11b8601ad7cd10c7b8909719b0642171858
jaimeBalseiro <jaime.diaz0303@gmail.com>
2022-09-02 20:13:21 +0000

    fixed for betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,6 +11,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	return (node->parent->left == 
+	return (node->parent->left ==
 			node ? node->parent->right : node->parent->left);
 }
